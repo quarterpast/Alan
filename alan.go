0d8909ed1e86f5c442837a47ed88b7f63f2b772c
Matt Brennan <mattyb1000@gmail.com>
2011-08-21 11:36:09 +0100

    gofmt

--- alan.go
+++ alan.go
@@ -8,12 +8,12 @@ import (
 
 type Alan struct {
 	*gtk.GtkWindow
-	tabs *gtk.GtkNotebook
+	tabs    *gtk.GtkNotebook
 	toolbar *gtk.GtkHBox
 }
 
 func TabToolbar() *gtk.GtkHBox {
-	hbox := gtk.HBox(true,0)
+	hbox := gtk.HBox(true, 0)
 	return hbox
 }
 func _Alan() Alan {
@@ -24,33 +24,33 @@ func _Alan() Alan {
 	alan.toolbar = TabToolbar()
 
 	alan.tabs.SetShowTabs(false)
-	alan.tabs.Connect("change-current-page",func(pos int) {
+	alan.tabs.Connect("change-current-page", func(pos int) {
 		tab := alan.tabs.GetNthPage(pos)
 		alan.SetTitle(alan.tabs.GetMenuLabelText(tab))
 	})
-	alan.tabs.Connect("page-removed",func() {
+	alan.tabs.Connect("page-removed", func() {
 		if alan.tabs.GetNPages() == 1 {
 			alan.tabs.SetShowTabs(false)
 		}
 	})
-	alan.tabs.Connect("page-added",func() {
+	alan.tabs.Connect("page-added", func() {
 		if alan.tabs.GetNPages() > 1 {
 			alan.tabs.SetShowTabs(true)
 		}
 	})
-	alan.tabs.SetActionWidget(alan.toolbar,gtk.GTK_PACK_END)
-	alan.SetDefaultSize(400,500)
-	alan.Connect("destroy",gtk.MainQuit)
+	alan.tabs.SetActionWidget(alan.toolbar, gtk.GTK_PACK_END)
+	alan.SetDefaultSize(400, 500)
+	alan.Connect("destroy", gtk.MainQuit)
 	alan.Add(alan.tabs)
 	return alan
 }
 func (a Alan) OpenDialog() {
-	dialog := gtk.FileChooserDialog("Open",a.GtkWindow,gtk.GTK_FILE_CHOOSER_ACTION_OPEN,"Open",1)
+	dialog := gtk.FileChooserDialog("Open", a.GtkWindow, gtk.GTK_FILE_CHOOSER_ACTION_OPEN, "Open", 1)
 	dialog.ShowAll()
 }
 func (a Alan) Open(files []string) {
-	for _,f := range files {
-		fmt.Printf("%d",a.tabs.AppendPage(gtk.Label(f),gtk.Label(f)));
+	for _, f := range files {
+		fmt.Printf("%d", a.tabs.AppendPage(gtk.Label(f), gtk.Label(f)))
 	}
 }
 func main() {
@@ -58,7 +58,7 @@ func main() {
 	defer gtk.Main()
 
 	app := _Alan()
-	if(len(os.Args) > 1) {
+	if len(os.Args) > 1 {
 		app.Open(os.Args[1:])
 	}
 	app.ShowAll()
